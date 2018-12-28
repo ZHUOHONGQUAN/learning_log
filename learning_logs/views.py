d136df2016f4709da3674186f642f507a6cc5c86
shkjdafhld <shkjdafhld@gmail.com>
2018-12-28 23:29:34 +0100

    change the font in h3 on index.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Topic, Entry
 
@@ -35,7 +35,7 @@ def topics(request):
 @login_required	
 def topic(request, topic_id):
 	
-	topic = Topic.objects.get(id=topic_id)
+	topic = get_object_or_404(Topic, id=topic_id)
 	
 	if topic.owner != request.user:
 		raise Http404
